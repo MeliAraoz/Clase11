d97cf4cb99dfe0f4f2a25e86a7d1325279a5c68c
MeliAraoz <melii.araoz96@gmail.com>
2023-05-06 23:33:16 -0300

    props

---
+++ clase_11/src/components/saludo.js
@@ -0,0 +1,9 @@
+export default function Saludo( { texto } ) {
+    return(
+        <div>
+            <h2>
+                Esto es un saludo para: { texto }
+            </h2>
+        </div>
+    );
+}
--- clase_11/src/pages/index.js
+++ clase_11/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Saludo from '@/components/saludo'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <Saludo texto="Melisa" edad="26"></Saludo>
       </main>
     </>
-  )
+  );
 }
